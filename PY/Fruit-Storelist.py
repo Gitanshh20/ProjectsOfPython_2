6c7e4c9d611920d306766d09375a34230ac2cb44
GitanshDev <gitanshagalcha@gmail.com>
2012-01-14 20:46:59 +0530

    PY/Fruit-Storelist.py

---
+++ PY/Fruit-Storelist.py
@@ -0,0 +1,29 @@
+#Program to store seven fruits in a list entered by the user.
+
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+fruits = []
+
+f1 = input("Enter Your Fruit Name: ")
+(fruits.append(f1))
+
+f2 = input("Enter Your Fruit Name: ")
+(fruits.append(f2))
+
+f3 = input("Enter Your Fruit Name: ")
+(fruits.append(f3))
+
+f4 = input("Enter Your Fruit Name: ")
+(fruits.append(f4))
+
+f5 = input("Enter Your Fruit Name: ")
+(fruits.append(f5))
+
+f6 = input("Enter Your Fruit Name: ")
+(fruits.append(f6))
+
+f7 = input("Enter Your Fruit Name: ")
+(fruits.append(f7))
+
+print(fruits)

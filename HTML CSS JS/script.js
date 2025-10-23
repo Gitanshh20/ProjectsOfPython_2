a0bfd90100d843d3bfad62bf79e0690adbaf0c78
GitanshDev <gitanshagalcha@gmail.com>
2025-10-23 12:23:22 +0530

    My_port.html

---
+++ HTML CSS JS/script.js
@@ -0,0 +1,1 @@
+console.log("Helllo World What's Happening")
---
+++ PYTHON C++/Gitansh.py
@@ -0,0 +1,19 @@
+# Making Coffe Bar Using If/else Conditional Staements.
+
+user = input("Which Coffe did You like:- ")
+
+cof_1 = "Cappuciano"
+cof_2 = "Cold Breze"
+cof_3 = "Hot Coffee"
+
+if(user == cof_1):
+    print("Great! Chocie It's Only $50")
+
+elif(user == cof_2):
+    print("Great! Chocie It's Only $30")
+
+elif(user == cof_3):
+    print("Great! Chocie It's Only $40")
+
+else:
+    print("Soryy We did not buy this coffee😓")
